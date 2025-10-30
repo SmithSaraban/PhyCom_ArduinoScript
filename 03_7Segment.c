9d1a830a0c3e6c456cdd9cf6d5beb254cf83ce42
SmithSaraban <smithsaraban@gmail.com>
2025-10-30 16:30:54 +0700

    Update 03_7Segment.c

--- 03_7Segment.c
+++ 03_7Segment.c
@@ -1,50 +1,47 @@
-int num_array[10][7] = {  { 1,1,1,1,1,1,0 },    // 0
-                          { 0,1,1,0,0,0,0 },    // 1
-                          { 1,1,0,1,1,0,1 },    // 2
-                          { 1,1,1,1,0,0,1 },    // 3
-                          { 0,1,1,0,0,1,1 },    // 4
-                          { 1,0,1,1,0,1,1 },    // 5
-                          { 1,0,1,1,1,1,1 },    // 6
-                          { 1,1,1,0,0,0,0 },    // 7
-                          { 1,1,1,1,1,1,1 },    // 8
-                          { 1,1,1,0,0,1,1 }};   // 9
+int num[10][7]{
+  {1,1,1,1,1,1,0},
+  {0,1,1,0,0,0,0},
+  {1,1,0,1,1,0,1},
+  {1,1,1,1,0,0,1},
+  {0,1,1,0,0,1,1},
+  {1,0,1,1,0,1,1},
+  {1,0,1,1,1,1,1},
+  {1,1,1,0,0,0,0},
+  {1,1,1,1,1,1,1},
+  {1,1,1,0,0,1,1},
+};
 
-// Define the pins for each segment of the 7-segment display
-const int segment_a = 2; 
-const int segment_b = 3; 
-const int segment_c = 4;
-const int segment_d = 5;
-const int segment_e = 6;
-const int segment_f = 7;
-const int segment_g = 8;
+const int seg_a = 2;
+const int seg_b = 3;
+const int seg_c = 4;
+const int seg_d = 5;
+const int seg_e = 6;
+const int seg_f = 7;
+const int seg_g = 8;
 
-void setup() { 
-  // set pin modes for each segment
-    pinMode(segment_a, OUTPUT);   
-    pinMode(segment_b, OUTPUT);   
-    pinMode(segment_c, OUTPUT);   
-    pinMode(segment_d, OUTPUT);   
-    pinMode(segment_e, OUTPUT);   
-    pinMode(segment_f, OUTPUT);   
-    pinMode(segment_g, OUTPUT);   
+void setup(){
+  pinMode(seg_a, OUTPUT);
+  pinMode(seg_b, OUTPUT);
+  pinMode(seg_c, OUTPUT);
+  pinMode(seg_d, OUTPUT);
+  pinMode(seg_e, OUTPUT);
+  pinMode(seg_f, OUTPUT);
+  pinMode(seg_g, OUTPUT);
 }
 
-void loop() {
-  // Loop through each digit (0-9)
-    for (int digit = 0; digit < 10; digit++) {
-        displayDigit(digit);
-        delay(1000); // Wait for 1 second before displaying the next digit
-    }
+void loop(){
+  for(int i = 0; i < 10; i++){
+    printNum(i);
+    delay(1000);
+  }
 }
 
-// Function to display a specific digit on the 7-segment display
-void displayDigit(int digit) {
-  // Set each segment according to the num_array for the given digit
-    digitalWrite(segment_a, num_array[digit][0]);
-    digitalWrite(segment_b, num_array[digit][1]);
-    digitalWrite(segment_c, num_array[digit][2]);
-    digitalWrite(segment_d, num_array[digit][3]);
-    digitalWrite(segment_e, num_array[digit][4]);
-    digitalWrite(segment_f, num_array[digit][5]);
-    digitalWrite(segment_g, num_array[digit][6]);
+void printNum(int i){
+  digitalWrite(seg_a, num[i][0]);
+  digitalWrite(seg_b, num[i][1]);
+  digitalWrite(seg_c, num[i][2]);
+  digitalWrite(seg_d, num[i][3]);
+  digitalWrite(seg_e, num[i][4]);
+  digitalWrite(seg_f, num[i][5]);
+  digitalWrite(seg_g, num[i][6]);
 }
