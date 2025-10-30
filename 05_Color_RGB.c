9a4713e08ed4152191af94ae5cf735d2d94c69ed
SmithSaraban <smithsaraban@gmail.com>
2025-10-30 17:05:01 +0700

    Update 05_Color_RGB.c

--- 05_Color_RGB.c
+++ 05_Color_RGB.c
@@ -1,44 +1,40 @@
-const int BTN_RED = 2;
-const int BTN_GREEN = 3;
-const int BTN_BLUE = 4;
+int btnRed = 2;
+int btnGreen = 3;
+int btnBlue = 4;
+int red = 5;
+int green = 6;
+int blue = 7;
 
-const int PIN_RED = 5;
-const int PIN_GREEN = 6;
-const int PIN_BLUE = 7;
+boolean redStat = true;
+boolean greenStat = true;
+boolean blueStat = true;
 
-bool is_red_on = false;
-bool is_green_on = false;
-bool is_blue_on = false;
+void setup(){
+  pinMode(btnRed, INPUT);
+  pinMode(btnGreen, INPUT);
+  pinMode(btnBlue, INPUT);
+  pinMode(red, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(blue, OUTPUT);
 
-void setup() {
-    pinMode(BTN_RED, INPUT_PULLUP);
-    pinMode(BTN_GREEN, INPUT_PULLUP);
-    pinMode(BTN_BLUE, INPUT_PULLUP);
-    
-    pinMode(PIN_RED, OUTPUT);
-    pinMode(PIN_GREEN, OUTPUT);
-    pinMode(PIN_BLUE, OUTPUT);
-
-    digitalWrite(PIN_RED, 1);
-    digitalWrite(PIN_GREEN, 1);
-    digitalWrite(PIN_BLUE, 1);
+  digitalWrite(red, HIGH);
+  digitalWrite(green, HIGH);
+  digitalWrite(blue, HIGH);
 }
 
-void loop() {
-    change_stat(BTN_RED, PIN_RED, &is_red_on);
-    change_stat(BTN_GREEN, PIN_GREEN, &is_green_on);
-    change_stat(BTN_BLUE, PIN_BLUE, &is_blue_on);
-    delay(5);
+void loop(){
+  change(btnRed, red, &redStat);
+  change(btnGreen, green, &greenStat);
+  change(btnBlue, blue, &blueStat);
+  delay(5);
 }
 
-void change_stat(int btn_pin, int led_pin, bool* status) {
-    if (digitalRead(btn_pin) == LOW) {
-        *status = !(*status);
-        if (*status) {
-        digitalWrite(led_pin, LOW);
-        } else {
-        digitalWrite(led_pin, HIGH);
-        }
-        delay(500);
-    }
+void change(int btn, int color, boolean* stat){
+  if(digitalRead(btn) == LOW) 
+  {
+    *stat = !(*stat);
+    if(*stat) digitalWrite(color, LOW);
+    else digitalWrite(color, HIGH);
+    delay(300);
+  }
 }
